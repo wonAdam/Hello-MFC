ee29188f36aa3b05081e43ea29caf017cb0e0f93
wonAdam <ehdgus5500@gmail.com>
2021-02-07 18:24:17 +0900

    init

---
+++ HelloSDK/HelloSDK/HelloSDK.cpp
@@ -0,0 +1,27 @@
+﻿#include <Windows.h>
+
+// WinMain 함수에서 참조하므로 함수 원형을 선언합니다.
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	WNDCLASS wndclass;
+	HWND hwnd;
+	MSG msg;
+
+	// 윈도우 클래스를 초기화하고 운영체제를 등록합니다.
+	wndclass.style = CS_HREDRAW | CS_VREDRAW;
+	wndclass.lpfnWndProc = WndProc;
+	wndclass.cbWndExtra = 0;
+	wndclass.hInstance = hInstance;
+	wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndclass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wndclass.lpszMenuName = NULL;
+	wndclass.lpszClassName = TEXT("HelloClass");
+	if (!RegisterClass(&wndclass)) return 1;
+
+	// 윈도우를 생성하고 화면에 나타낸다.
+	//hwnd = CreateWindow(TEXT("HelloClass"))
+
+}
