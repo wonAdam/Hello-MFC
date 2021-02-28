b56669a1d69a7b8a174a52c4868f1b4019f3aef0
wonAdam <ehdgus5500@gmail.com>
2021-02-28 10:17:13 +0900

    DrawCircles

---
+++ DrawCircles/ChildView.cpp
@@ -0,0 +1,112 @@
+﻿
+// ChildView.cpp : implementation of the CChildView class
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "DrawCircles.h"
+#include "ChildView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CChildView
+
+CChildView::CChildView()
+{
+	m_bDrawMode = FALSE;
+}
+
+CChildView::~CChildView()
+{
+}
+
+
+BEGIN_MESSAGE_MAP(CChildView, CWnd)
+	ON_WM_PAINT()
+	ON_WM_MOUSEMOVE()
+	ON_WM_LBUTTONDOWN()
+	ON_WM_LBUTTONUP()
+END_MESSAGE_MAP()
+
+
+
+// CChildView message handlers
+
+BOOL CChildView::PreCreateWindow(CREATESTRUCT& cs) 
+{
+	if (!CWnd::PreCreateWindow(cs))
+		return FALSE;
+
+	cs.dwExStyle |= WS_EX_CLIENTEDGE;
+	cs.style &= ~WS_BORDER;
+	cs.lpszClass = AfxRegisterWndClass(CS_HREDRAW|CS_VREDRAW|CS_DBLCLKS, 
+		::LoadCursor(nullptr, IDC_ARROW), reinterpret_cast<HBRUSH>(COLOR_WINDOW+1), nullptr);
+
+	return TRUE;
+}
+
+void CChildView::OnPaint() 
+{
+	CPaintDC dc(this); 
+
+}
+
+
+
+void CChildView::OnLButtonDown(UINT nFlags, CPoint point)
+{
+	// TODO: Add your message handler code here and/or call default
+	m_bDrawMode = TRUE;
+	SetCapture();
+
+	m_x1 = m_x2 = point.x;
+	m_y1 = m_y2 = point.y;
+
+	CWnd::OnLButtonDown(nFlags, point);
+}
+
+void CChildView::OnMouseMove(UINT nFlags, CPoint point)
+{
+	// TODO: Add your message handler code here and/or call default
+	if (m_bDrawMode)
+	{
+		CClientDC dc(this);
+		dc.SelectStockObject(NULL_BRUSH);
+
+		// 이전의 타원을 지운다.
+		dc.SetROP2(R2_NOT);
+		dc.Ellipse(m_x1, m_y1, m_x2, m_y2);
+
+		// 새로운 타원을 그린다.
+		dc.SetROP2(R2_NOT);
+		m_x2 = point.x;
+		m_y2 = point.y;
+		dc.Ellipse(m_x1, m_y1, m_x2, m_y2);
+	}
+
+	CWnd::OnMouseMove(nFlags, point);
+}
+
+
+void CChildView::OnLButtonUp(UINT nFlags, CPoint point)
+{
+	// TODO: Add your message handler code here and/or call default
+	CClientDC dc(this);
+	dc.SelectStockObject(NULL_BRUSH);
+
+	// 최종적인 타원을 그린다.
+	dc.SetROP2(R2_COPYPEN);
+	m_x2 = point.x;
+	m_y2 = point.y;
+	dc.Ellipse(m_x1, m_y1, m_x2, m_y2);
+
+	// 그리기 모드를 끝낸다.
+	m_bDrawMode = FALSE;
+
+	::ReleaseCapture();
+
+	CWnd::OnLButtonUp(nFlags, point);
+}
