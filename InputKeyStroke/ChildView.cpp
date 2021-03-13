aad9794d27e0de27b7b06e4bb3cccd5098231e1e
AdamWon <ehdgus5500@gmail.com>
2021-03-13 19:30:28 +0900

    InputKeyStroke

---
+++ InputKeyStroke/ChildView.cpp
@@ -0,0 +1,93 @@
+
+// ChildView.cpp : implementation of the CChildView class
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "InputKeyStroke.h"
+#include "ChildView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CChildView
+
+CChildView::CChildView()
+{
+	m_xPos = m_yPos = 60;
+	m_bFill = FALSE;
+}
+
+CChildView::~CChildView()
+{
+}
+
+
+BEGIN_MESSAGE_MAP(CChildView, CWnd)
+	ON_WM_PAINT()
+	ON_WM_SIZE()
+	ON_WM_KEYDOWN()
+END_MESSAGE_MAP()
+
+
+
+// CChildView message handlers
+
+BOOL CChildView::PreCreateWindow(CREATESTRUCT& cs) 
+{
+	if (!CWnd::PreCreateWindow(cs))
+		return FALSE;
+
+	cs.dwExStyle |= WS_EX_CLIENTEDGE;
+	cs.style &= ~WS_BORDER;
+	cs.lpszClass = AfxRegisterWndClass(CS_HREDRAW|CS_VREDRAW|CS_DBLCLKS, 
+		::LoadCursor(nullptr, IDC_ARROW), reinterpret_cast<HBRUSH>(COLOR_WINDOW+1), nullptr);
+
+	return TRUE;
+}
+
+void CChildView::OnPaint() 
+{
+	CPaintDC dc(this); 
+	if (m_bFill == TRUE) dc.SelectStockObject(BLACK_BRUSH);
+	dc.Ellipse(m_xPos - 20, m_yPos - 20, m_xPos + 20, m_yPos + 20);
+}
+
+
+void CChildView::OnSize(UINT nType, int cx, int cy)
+{
+	m_xMax = cx;
+	m_yMax = cy;
+}
+
+
+void CChildView::OnKeyDown(UINT nChar, UINT nRepCnt, UINT nFlags)
+{
+	switch (nChar)
+	{
+	case VK_LEFT:
+		m_xPos -= 20;
+		break;
+
+	case VK_RIGHT:
+		m_xPos += 20;
+		break;
+
+	case VK_UP:
+		m_yPos -= 20;
+		break;
+
+	case VK_DOWN:
+		m_yPos += 20;
+		break;
+
+	case VK_SPACE:
+		m_bFill = !m_bFill;
+	}
+
+	m_xPos = min(max(20, m_xPos), m_xMax - 20);
+	m_yPos = min(max(20, m_yPos), m_yMax - 20);
+	Invalidate();
+}
---
+++ InputKeyStroke/ChildView.h
@@ -0,0 +1,42 @@
+
+// ChildView.h : interface of the CChildView class
+//
+
+
+#pragma once
+
+
+// CChildView window
+
+class CChildView : public CWnd
+{
+// Construction
+public:
+	CChildView();
+
+// Attributes
+public:
+	int m_xPos, m_yPos;
+	int m_xMax, m_yMax;
+	BOOL m_bFill;
+
+// Operations
+public:
+
+// Overrides
+	protected:
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+
+// Implementation
+public:
+	virtual ~CChildView();
+
+	// Generated message map functions
+protected:
+	afx_msg void OnPaint();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnSize(UINT nType, int cx, int cy);
+	afx_msg void OnKeyDown(UINT nChar, UINT nRepCnt, UINT nFlags);
+};
+
